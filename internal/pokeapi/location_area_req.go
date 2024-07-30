7fde7478d2d08106e15e80e22c5d7f6b5f480288
jakebourdow <jakebourdow@gmail.com>
2024-07-30 16:32:16 -0600

    Add cacheing to map and mapb

--- internal/pokeapi/location_area_req.go
+++ internal/pokeapi/location_area_req.go
@@ -14,6 +14,21 @@ func (c *Client) ListLocationAreas(pageURL *string) (LocationAreasResp, error) {
 		fullURL = *pageURL
 	}
 
+	// check the cache
+	dat, ok := c.cache.Get(fullURL)
+	if ok {
+		// cache hit
+		fmt.Println("cache hit!")
+		locationAreasResp := LocationAreasResp{}
+		err := json.Unmarshal(dat, &locationAreasResp)
+		if err != nil {
+			return LocationAreasResp{}, err
+		}
+
+		return locationAreasResp, nil
+	}
+	fmt.Println("cache miss!")
+
 	req, err := http.NewRequest("GET", fullURL, nil)
 	if err != nil {
 		return LocationAreasResp{}, err
@@ -29,7 +44,7 @@ func (c *Client) ListLocationAreas(pageURL *string) (LocationAreasResp, error) {
 		return LocationAreasResp{}, fmt.Errorf("bad status code: %v", resp.StatusCode)
 	}
 
-	dat, err := io.ReadAll(resp.Body)
+	dat, err = io.ReadAll(resp.Body)
 	if err != nil {
 		return LocationAreasResp{}, err
 	}
@@ -39,6 +54,8 @@ func (c *Client) ListLocationAreas(pageURL *string) (LocationAreasResp, error) {
 	if err != nil {
 		return LocationAreasResp{}, err
 	}
+
+	c.cache.Add(fullURL, dat)
 
 	return locationAreasResp, nil
 }
--- internal/pokeapi/pokeapi.go
+++ internal/pokeapi/pokeapi.go
@@ -3,16 +3,20 @@ package pokeapi
 import (
 	"net/http"
 	"time"
+
+	"github.com/jakebourdow/pokedexcli/internal/pokecahe"
 )
 
 const baseURL = "https://pokeapi.co/api/v2"
 
 type Client struct {
+	cache      pokecahe.Cache
 	httpClient http.Client
 }
 
-func NewClient() Client {
+func NewClient(cacheInterval time.Duration) Client {
 	return Client{
+		cache: pokecahe.NewCache(cacheInterval),
 		httpClient: http.Client{
 			Timeout: time.Minute,
 		},
