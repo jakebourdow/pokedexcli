0742733a07533ada444c59f9dfc4e6cdfe163608
jakebourdow <jakebourdow@gmail.com>
2025-06-17 16:53:52 -0700

    update format of commands

--- command_help.go
+++ command_help.go
@@ -3,14 +3,14 @@ package main
 import "fmt"
 
 func commandHelp() error {
-	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("\nWelcome to the Pokedex!")
 	fmt.Print("Usage:\n\n")
 	availableCommands := getCommands()
 	if len(availableCommands) == 0 {
 		return fmt.Errorf("no commands available")
 	}
-	for i := range availableCommands {
-		fmt.Printf("%s: %s\n", availableCommands[i].name, availableCommands[i].description)
+	for _, cmd := range getCommands() {
+		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
 	}
 	return nil
 }
