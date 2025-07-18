abb380dd11fc2f3251ad8db33477138821b71783
jakebourdow <jakebourdow@gmail.com>
2025-07-18 14:48:16 -0700

    create cacheing

--- command_help.go
+++ command_help.go
@@ -2,13 +2,11 @@ package main
 
 import "fmt"
 
-func commandHelp() error {
-	fmt.Println("\nWelcome to the Pokedex!")
-	fmt.Print("Usage:\n\n")
-	availableCommands := getCommands()
-	if len(availableCommands) == 0 {
-		return fmt.Errorf("no commands available")
-	}
+func commandHelp(cfg *config) error {
+	fmt.Println()
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	fmt.Println()
 	for _, cmd := range getCommands() {
 		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
 	}
--- repl.go
+++ repl.go
@@ -5,11 +5,18 @@ import (
 	"fmt"
 	"os"
 	"strings"
+
+	"github.com/jakebourdow/pokedexcli/internal/pokeapi"
 )
 
-func startRepl() {
-	reader := bufio.NewScanner(os.Stdin)
+type config struct {
+	pokeapiClient    pokeapi.Client
+	nextLocationsURL *string
+	prevLocationsURL *string
+}
 
+func startRepl(cfg *config) {
+	reader := bufio.NewScanner(os.Stdin)
 	for {
 		fmt.Print("Pokedex > ")
 		reader.Scan()
@@ -18,21 +25,33 @@ func startRepl() {
 		if len(words) == 0 {
 			continue
 		}
+
 		commandName := words[0]
-		availableCommands := getCommands()
-		command, ok := availableCommands[commandName]
-		if !ok {
-			fmt.Print("Unkown command\n")
+
+		command, exists := getCommands()[commandName]
+		if exists {
+			err := command.callback(cfg)
+			if err != nil {
+				fmt.Println(err)
+			}
+			continue
+		} else {
+			fmt.Println("Unknown command")
 			continue
 		}
-		command.callback()
 	}
+}
+
+func cleanInput(text string) []string {
+	output := strings.ToLower(text)
+	words := strings.Fields(output)
+	return words
 }
 
 type cliCommand struct {
 	name        string
 	description string
-	callback    func() error
+	callback    func(*config) error
 }
 
 func getCommands() map[string]cliCommand {
@@ -42,16 +61,20 @@ func getCommands() map[string]cliCommand {
 			description: "Displays a help message",
 			callback:    commandHelp,
 		},
+		"map": {
+			name:        "map",
+			description: "Get the next page of locations",
+			callback:    commandMapf,
+		},
+		"mapb": {
+			name:        "mapb",
+			description: "Get the previous page of locations",
+			callback:    commandMapb,
+		},
 		"exit": {
 			name:        "exit",
 			description: "Exit the pokedex",
 			callback:    commandExit,
 		},
 	}
-}
-
-func cleanInput(text string) []string {
-	lowered := strings.ToLower(text)
-	words := strings.Fields(lowered)
-	return words
 }
